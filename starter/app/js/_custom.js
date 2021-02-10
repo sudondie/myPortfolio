da38e27427b545ae1dae1ea70f6afb646e010b8c
sudondie <ilia.skakov@gmail.com>
2021-02-10 18:03:03 +0300

    adapted first 2 pages

--- starter/app/js/_custom.js
+++ starter/app/js/_custom.js
@@ -1,37 +1,58 @@
 document.addEventListener("DOMContentLoaded", function () {
-    $('.row__burger').click(function(event){
+    $('.row__burger').click(function (event) {
         $('.row__burger,.header__menu,.header__description,.header__image,.aboutMe,body').toggleClass('active');
-        anime({
-            targets: '.header__menu.active',
-        });
     });
+    let htmlimage = document.querySelector('.header__image');
+    let menuButton = document.querySelector('.row__burger');
+    let menuList = document.querySelector('.header__menu');
+    let animationHTML = anime({
+        targets: htmlimage,
+        keyframes: [{
+                scale: 1.1
+            },
+            {
+                rotate: 360,
+                duration: 2000
+            },
+            {
+                scale: 1
+            },
+        ],
+        autoplay: false,
+        duration: 1500,
+    });
+    let animationMenu = anime({
+        targets: menuList,
+        scale: 1.1,
+        duration: 1000,
+    })
+    htmlimage.onmouseover = animationHTML.play;
+    menuButton.onclick = animationMenu.play;
 
-    anime({
-        targets: '.header__image',
-        duration: 2000,
-    });
+
+
     $('#pagepiling').pagepiling({
-	    menu: null,
+        menu: null,
         direction: 'vertical',
         verticalCentered: true,
         sectionsColor: [],
         anchors: [],
-        scrollingSpeed: 600,
-        easing: 'swing',
-        loopBottom: true,
+        scrollingSpeed: 400,
+        easing: 'linear',
+        loopBottom: false,
         loopTop: false,
         css3: true,
         navigation: false,
-       	normalScrollElements: null,
+        normalScrollElements: null,
         normalScrollElementTouchThreshold: 5,
         touchSensitivity: 5,
         keyboardScrolling: true,
         sectionSelector: '.section',
         animateAnchor: false,
 
-		//events
-		onLeave: function(index, nextIndex, direction){},
-		afterLoad: function(anchorLink, index){},
-		afterRender: function(){},
-	});
+        //events
+        onLeave: function (index, nextIndex, direction) {},
+        afterLoad: function (anchorLink, index) {},
+        afterRender: function () {},
+    });
 });
--- starter/gulpfile.js
+++ starter/gulpfile.js
@@ -1,98 +1,126 @@
-var gulp         	 = require('gulp'),
-		sass         = require('gulp-sass'),
-		browserSync  = require('browser-sync').create(),
-		concat       = require('gulp-concat'),
-		uglify       = require('gulp-uglify-es').default,
-		cleancss     = require('gulp-clean-css'),
-		autoprefixer = require('gulp-autoprefixer'),
-		rsync        = require('gulp-rsync'),
-		newer        = require('gulp-newer'),
-		rename       = require('gulp-rename'),
-		responsive   = require('gulp-responsive'),
-		del          = require('del');
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync').create(),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify-es').default,
+	cleancss = require('gulp-clean-css'),
+	autoprefixer = require('gulp-autoprefixer'),
+	rsync = require('gulp-rsync'),
+	newer = require('gulp-newer'),
+	rename = require('gulp-rename'),
+	responsive = require('gulp-responsive'),
+	del = require('del');
 
 // Local Server
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
 	browserSync.init({
 		server: {
-			baseDir: 'app'
+			baseDir: 'app/'
 		},
-		notify: false,
-		// online: false, // Work offline without internet connection
-		// tunnel: true, tunnel: 'projectname', // Demonstration page: http://projectname.localtunnel.me
-	})
+		tunnel: true
+	});
 });
-function bsReload(done) { browserSync.reload(); done(); }; //bs reload function
+
+function bsReload(done) {
+	browserSync.reload();
+	done();
+}; //bs reload function
 
 // Custom Styles
-gulp.task('styles', function() {
+gulp.task('styles', function () {
 	return gulp.src('app/sass/**/*.sass')
-	.pipe(sass({
-		outputStyle: 'expanded',
-		includePaths: [__dirname + '/node_modules']
-	}))
-	.pipe(concat('styles.min.css'))
-	.pipe(autoprefixer({
-		grid: true,
-		overrideBrowserslist: ['last 10 versions']
-	}))
-	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Optional. Comment out when debugging
-	.pipe(gulp.dest('app/css'))
-	.pipe(browserSync.stream())
+		.pipe(sass({
+			outputStyle: 'expanded',
+			includePaths: [__dirname + '/node_modules']
+		}))
+		.pipe(concat('styles.min.css'))
+		.pipe(autoprefixer({
+			grid: true,
+			overrideBrowserslist: ['last 10 versions']
+		}))
+		.pipe(cleancss({
+			level: {
+				1: {
+					specialComments: 0
+				}
+			}
+		})) // Optional. Comment out when debugging
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.stream())
 });
 
 // Scripts & JS Libraries
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
 	return gulp.src([
-		'node_modules/jquery/dist/jquery.min.js',
-		'node_modules/animejs/lib/anime.min.js',
-		'node_modules/pagePiling.js/jquery.pagepiling.js',// Optional jQuery plug-in (npm i --save-dev jquery)
-		'app/js/_libs.js', // JS libraries (all in one)
-		'app/js/_custom.js', // Custom scripts. Always at the end
+			'node_modules/jquery/dist/jquery.min.js',
+			'node_modules/animejs/lib/anime.min.js',
+			'node_modules/pagePiling.js/jquery.pagepiling.js', // Optional jQuery plug-in (npm i --save-dev jquery)
+			'app/js/_libs.js', // JS libraries (all in one)
+			'app/js/_custom.js', // Custom scripts. Always at the end
 		])
-	.pipe(concat('scripts.min.js'))
-	.pipe(uglify()) // Minify js (opt.)
-	.pipe(gulp.dest('app/js'))
-	.pipe(browserSync.reload({ stream: true }))
+		.pipe(concat('scripts.min.js'))
+		.pipe(uglify()) // Minify js (opt.)
+		.pipe(gulp.dest('app/js'))
+		.pipe(browserSync.reload({
+			stream: true
+		}))
 });
 
 // Responsive Images
 var quality = 95; // Responsive images quality
 
 // Produce @1x images
-gulp.task('img-responsive-1x', async function() {
+gulp.task('img-responsive-1x', async function () {
 	return gulp.src('app/img/_src/**/*.{png,jpg,jpeg,webp,raw}')
 		.pipe(newer('app/img/@1x'))
 		.pipe(responsive({
-			'**/*': { width: '50%', quality: quality }
-		})).on('error', function (e) { console.log(e) })
-		.pipe(rename(function (path) {path.extname = path.extname.replace('jpeg', 'jpg')}))
+			'**/*': {
+				width: '50%',
+				quality: quality
+			}
+		})).on('error', function (e) {
+			console.log(e)
+		})
+		.pipe(rename(function (path) {
+			path.extname = path.extname.replace('jpeg', 'jpg')
+		}))
 		.pipe(gulp.dest('app/img/@1x'))
 });
 // Produce @2x images
-gulp.task('img-responsive-2x', async function() {
+gulp.task('img-responsive-2x', async function () {
 	return gulp.src('app/img/_src/**/*.{png,jpg,jpeg,webp,raw}')
 		.pipe(newer('app/img/@2x'))
 		.pipe(responsive({
-			'**/*': { width: '100%', quality: quality }
-		})).on('error', function (e) { console.log(e) })
-		.pipe(rename(function (path) {path.extname = path.extname.replace('jpeg', 'jpg')}))
+			'**/*': {
+				width: '100%',
+				quality: quality
+			}
+		})).on('error', function (e) {
+			console.log(e)
+		})
+		.pipe(rename(function (path) {
+			path.extname = path.extname.replace('jpeg', 'jpg')
+		}))
 		.pipe(gulp.dest('app/img/@2x'))
 });
 gulp.task('img', gulp.series('img-responsive-1x', 'img-responsive-2x', bsReload));
 
 // Clean @*x IMG's
-gulp.task('cleanimg', function() {
-	return del(['app/img/@*'], { force: true })
+gulp.task('cleanimg', function () {
+	return del(['app/img/@*'], {
+		force: true
+	})
 });
 
 // Code & Reload
-gulp.task('code', function() {
+gulp.task('code', function () {
 	return gulp.src('app/**/*.html')
-	.pipe(browserSync.reload({ stream: true }))
+		.pipe(browserSync.reload({
+			stream: true
+		}))
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
 	gulp.watch('app/sass/**/*.sass', gulp.parallel('styles'));
 	gulp.watch(['app/js/_custom.js', 'app/js/_libs.js'], gulp.parallel('scripts'));
 	gulp.watch('app/*.html', gulp.parallel('code'));
